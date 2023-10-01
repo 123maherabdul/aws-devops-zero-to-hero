9860e836eadf7e9feff6970667d0148525c3443b
123maherabdul <139633670+123maherabdul@users.noreply.github.com>
2023-10-01 15:56:50 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 
-docker pull abdulmaher/simple-python-app:latest
+docker pull abdulmaher/simple-python-app
 
 # Run the Docker image as a container
 docker run -it
