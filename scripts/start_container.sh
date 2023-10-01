024b90b84010ec9e7928735acc149e080110a6af
123maherabdul <139633670+123maherabdul@users.noreply.github.com>
2023-10-01 11:16:55 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull abhishekf5/simple-python-flask-app
+docker push abdulmaher/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 abhishekf5/simple-python-flask-app
+docker run -d -p 5000:5000 abdulmaher/simple-python-app
